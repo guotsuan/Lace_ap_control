6dae8d38836dae3885b0adefd67caf24a2345081
Quan Guo <guoquan@shao.ac.cn>
2022-10-24 09:04:05 +0000

    ready for upload

---
+++ control.py
@@ -0,0 +1,240 @@
+##################################################
+
+#           P26 ----> Relay_Ch1
+#			P20 ----> Relay_Ch2
+#			P21 ----> Relay_Ch3
+
+##################################################
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import RPi.GPIO as GPIO
+import time
+
+Relay_Ch1 = 26
+Relay_Ch2 = 20
+Relay_Ch3 = 21
+
+SPT1_Ch1 = 5
+SPT1_Ch2 = 6
+
+SPT2_Ch1 = 2
+SPT2_Ch2 = 3
+
+SPT3_Ch1 = 17
+SPT3_Ch2 = 27
+
+SPT4_Ch1 = 13
+SPT4_Ch2 = 19
+
+SPT5_Ch1 = 14
+SPT5_Ch2 = 15
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(Relay_Ch1,GPIO.OUT)
+GPIO.setup(Relay_Ch2,GPIO.OUT)
+GPIO.setup(Relay_Ch3,GPIO.OUT)
+
+print("Setup The Relay Module is [success]")
+
+GPIO.setup(SPT1_Ch1, GPIO.OUT)
+GPIO.setup(SPT1_Ch2, GPIO.OUT)
+
+print("Setup SPT1 Module is [success]")
+
+GPIO.setup(SPT2_Ch1, GPIO.OUT)
+GPIO.setup(SPT2_Ch2, GPIO.OUT)
+
+print("Setup SPT2 Module is [success]")
+
+GPIO.setup(SPT3_Ch1, GPIO.OUT)
+GPIO.setup(SPT3_Ch2, GPIO.OUT)
+
+print("Setup SPT3 Module is [success]")
+
+GPIO.setup(SPT4_Ch1, GPIO.OUT)
+GPIO.setup(SPT4_Ch2, GPIO.OUT)
+
+print("Setup SPT4 Module is [success]")
+
+GPIO.setup(SPT5_Ch1, GPIO.OUT)
+GPIO.setup(SPT5_Ch2, GPIO.OUT)
+
+print("Setup SPT4 Module is [success]")
+
+def all_relay_off():
+        GPIO.output(Relay_Ch1,GPIO.HIGH)
+        GPIO.output(Relay_Ch2,GPIO.HIGH)
+        GPIO.output(Relay_Ch3,GPIO.HIGH)
+
+def set_relay(num):
+    if num == 1:
+        GPIO.output(Relay_Ch1,GPIO.LOW)
+        print("Channel 1:The Common Contact is access to the Normal Open Contact!")
+        GPIO.output(Relay_Ch2,GPIO.HIGH)
+        GPIO.output(Relay_Ch3,GPIO.HIGH)
+        time.sleep(0.2)
+    elif num == 2:
+        GPIO.output(Relay_Ch2,GPIO.LOW)
+        print("Channel 2:The Common Contact is access to the Normal Open Contact!")
+        GPIO.output(Relay_Ch1,GPIO.HIGH)
+        GPIO.output(Relay_Ch3,GPIO.HIGH)
+        time.sleep(0.2)
+    elif num == 3:
+        GPIO.output(Relay_Ch3,GPIO.LOW)
+        print("Channel 3:The Common Contact is access to the Normal Closed Contact!\n")
+        GPIO.output(Relay_Ch1,GPIO.HIGH)
+        GPIO.output(Relay_Ch2,GPIO.HIGH)
+        time.sleep(0.2)
+
+def set_spt1(num):
+    """
+    num = {1, 2, 3, 4}
+    p1:  5 low, 6 high
+    p2:  5 low, 6 low
+    p3: 5 high, 6 high
+    p4: 5 hight 6 low
+    """
+
+    ch1 = SPT1_Ch1
+    ch2 = SPT1_Ch2
+
+    try:
+        if num == 1:
+            GPIO.output(ch1, GPIO.LOW)
+            GPIO.output(ch2, GPIO.HIGH)
+        elif num == 2:
+            GPIO.output(ch1, GPIO.LOW)
+            GPIO.output(ch2, GPIO.LOW)
+        elif num == 3:
+            GPIO.output(ch1, GPIO.HIGH)
+            GPIO.output(ch2, GPIO.HIGH)
+        elif num == 4:
+            GPIO.output(ch1, GPIO.HIGH)
+            GPIO.output(ch2, GPIO.LOW)
+
+        return True
+    except:
+        return False
+
+
+def set_spt2(num):
+    """
+    num = {1, 2, 3, 4}
+    """
+    try:
+        if num == 4:
+            GPIO.output(SPT2_Ch1, GPIO.LOW)
+            GPIO.output(SPT2_Ch2, GPIO.HIGH)
+        elif num == 2:
+            GPIO.output(SPT2_Ch1, GPIO.LOW)
+            GPIO.output(SPT2_Ch2, GPIO.LOW)
+        elif num == 3:
+            GPIO.output(SPT2_Ch1, GPIO.HIGH)
+            GPIO.output(SPT2_Ch2, GPIO.HIGH)
+        elif num == 1:
+            GPIO.output(SPT2_Ch1, GPIO.HIGH)
+            GPIO.output(SPT2_Ch2, GPIO.LOW)
+        return True
+    except:
+        return False
+
+def set_spt3(num):
+    """
+    num = {1, 2, 3, 4}
+    """
+    ch1 = SPT3_Ch1
+    ch2 = SPT3_Ch2
+
+    if num == 4:
+        GPIO.output(ch1, GPIO.LOW)
+        GPIO.output(ch2, GPIO.HIGH)
+    elif num ==2:
+        GPIO.output(ch1, GPIO.LOW)
+        GPIO.output(ch2, GPIO.LOW)
+    elif num ==3:
+        GPIO.output(ch1, GPIO.HIGH)
+        GPIO.output(ch2, GPIO.HIGH)
+    elif num == 1:
+        GPIO.output(ch1, GPIO.HIGH)
+        GPIO.output(ch2, GPIO.LOW)
+
+
+
+def set_spt4(num):
+    """
+    num = {1, 2, 3, 4}
+    """
+    ch1 = SPT4_Ch1
+    ch2 = SPT4_Ch2
+
+    if num == 4:
+        GPIO.output(ch1, GPIO.LOW)
+        GPIO.output(ch2, GPIO.HIGH)
+    elif num ==2:
+        GPIO.output(ch1, GPIO.LOW)
+        GPIO.output(ch2, GPIO.LOW)
+    elif num ==3:
+        GPIO.output(ch1, GPIO.HIGH)
+        GPIO.output(ch2, GPIO.HIGH)
+    elif num == 1:
+        GPIO.output(ch1, GPIO.HIGH)
+        GPIO.output(ch2, GPIO.LOW)
+
+
+def set_spt5(num):
+    """
+    num = {1, 2, 3, 4}
+    """
+    ch1 = SPT5_Ch1
+    ch2 = SPT5_Ch2
+
+    if num == 4:
+        GPIO.output(ch1, GPIO.LOW)
+        GPIO.output(ch2, GPIO.HIGH)
+    elif num ==2:
+        GPIO.output(ch1, GPIO.LOW)
+        GPIO.output(ch2, GPIO.LOW)
+    elif num ==3:
+        GPIO.output(ch1, GPIO.HIGH)
+        GPIO.output(ch2, GPIO.HIGH)
+    elif num == 1:
+        GPIO.output(ch1, GPIO.HIGH)
+        GPIO.output(ch2, GPIO.LOW)
+
+# all_relay_off()
+# set_relay(2)
+# set_spt3(4)
+# set_spt4(1)
+
+if __name__ == "__main__":
+    set_spt1(4)
+
+    # for Zhang 80DB
+    # set_relay(2)
+    # set_spt2(3)
+    # set_spt3(4)
+    # set_spt4(3)
+
+    # for Zang 60DB
+    # set_relay(2)
+    # set_spt2(3)
+    # set_spt3(1)
+    # set_spt4(1)
+
+    # for Guo 80DB myself
+
+    set_relay(3)
+    set_spt2(4)
+    set_spt4(2)
+
+    set_spt5(4)
+
+    # for Guo 80DB one
+
+    # set_relay(1)
+    # set_spt2(1)
+    # set_spt4(4)
+
+    # set_relay(3)
---
+++ main.py
@@ -0,0 +1,279 @@
+#! /usr/bin/env python3
+# vim:fenc=utf-8
+#
+# Copyright © 2022 gq <gq@laceap1>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+
+import flet
+from flet import Page, Text, Row, ElevatedButton,\
+    Container, colors, Column, ButtonStyle, Image, Dropdown, dropdown, \
+    FilledButton
+from flet.border import BorderSide
+from flet.buttons import RoundedRectangleBorder, CircleBorder
+from control import set_spt1, set_spt2, set_spt3, set_spt4, set_spt5, \
+    set_relay, all_relay_off
+
+option_changed = False
+input_sel = -1
+ap_sel = -1
+output_sel = -1
+ap_internal_sel = -1
+ap_out_sel = -1
+relay_sel = -1
+Is_input_selected = False
+Is_dev_selected = False
+Is_output_selected = False
+all_powr_down = True
+
+
+def main(page: Page):
+    # all_relay_off()
+
+    header_img = Image(
+        src="imgs/choose_wisely.jpeg",
+        width=620,
+        height=260,
+        fit="fitWidth"
+    )
+
+    page.title = "低频全天总功率实验控制系统"
+    page.horizontal_alignment = "center"
+    page.vertical_alignment = "center"
+
+    page_t = Text(page.title, size=70)
+
+    t1 = Text(value="输入：?", size=14)
+    t2 = Text(value="放大器: ?", size=14)
+    t3 = Text(value="输入：?", size=14)
+
+    def input_changed(e):
+        global Is_input_selected, input_sel, option_changed
+        t1.value = f"输入：{dd_input.value}"
+
+        option_changed=True
+
+        Is_input_selected = True
+        if "RF输入1" in t1.value:
+            input_sel = 4
+        elif "RF输入2" in t1.value:
+            input_sel = 3
+        elif "RF输入3" in t1.value:
+            input_sel = 2
+        elif "RF输入4" in t1.value:
+            input_sel = 1
+
+        power_up_button.icon="play_circle"
+        power_up_button.text="上电"
+        power_up_button.icon_color=colors.GREEN
+        page.update()
+
+    def ap_dev_changed(e):
+        global Is_dev_selected, ap_sel, ap_out_sel, ap_internal_sel, \
+            relay_sel, option_changed
+        t2.value=f"放大器：{dd_ap.value}"
+
+        option_changed=True
+        Is_dev_selected = True
+        if "1#" in t2.value:
+            ap_sel = 4
+            ap_out_sel = 2
+            ap_internal_sel = -1
+            relay_sel = 3
+        elif "2#" in t2.value:
+            ap_sel = 1
+            ap_out_sel = 4
+            ap_internal_sel = -1
+            relay_sel = 1
+        elif "3#" in t2.value:
+            ap_sel = 3
+            ap_out_sel = 1
+            ap_internal_sel = 1
+            relay_sel = 2
+        elif "4#" in t2.value:
+            ap_sel = 3
+            ap_out_sel = 3
+            ap_internal_sel = 4
+            relay_sel = 2
+        elif "5#" in t2.value:
+            ap_sel = 2
+            ap_out_sel = -1
+            ap_internal_sel = -1
+
+        power_up_button.icon="play_circle"
+        power_up_button.text="上电"
+        power_up_button.icon_color=colors.GREEN
+        page.update()
+
+    def output_changed(e):
+        global Is_output_selected, output_sel, option_changed
+        t3.value=f"输出：{dd_output.value}"
+
+        option_changed=True
+        Is_output_selected = True
+        if "输出1" in t3.value:
+            output_sel = 4
+        elif "输出2" in t3.value:
+            output_sel = 3
+        else:
+            output_sel = -1
+
+        power_up_button.icon="play_circle"
+        power_up_button.text="上电"
+        power_up_button.icon_color=colors.GREEN
+        page.update()
+
+    dd_input = Dropdown(
+        on_change=input_changed,
+        label="输入选择",
+        width=200,
+        options=[
+            dropdown.Option("RF输入1(带低噪放)"),
+            dropdown.Option("RF输入2"),
+            dropdown.Option("RF输入3"),
+            dropdown.Option("RF输入4(RF+DC)"),
+        ],
+        autofocus=True
+
+    )
+
+    dd_ap = Dropdown(
+        on_change=ap_dev_changed,
+        label="放大器选择",
+        width=200,
+        options=[
+            dropdown.Option("1# LACE自制"),
+            dropdown.Option("2# LACE定制"),
+            dropdown.Option("3# 西电60DB"),
+            dropdown.Option("4# 西电80DB"),
+            dropdown.Option("5# 矢网R60")
+        ],
+        autofocus=True
+    )
+
+    dd_output = Dropdown(
+        on_change=output_changed,
+        label="输出选择",
+        width=200,
+        options=[
+            dropdown.Option("输出1"),
+            dropdown.Option("输出2(不带滤波)"),
+            dropdown.Option("矢网测量无输出"),
+        ],
+        autofocus=True
+    )
+    ap_input = Container(content=Column([
+        dd_input, t1]),
+         bgcolor=colors.GREEN,
+         padding=20,
+         width=240,
+         height=150)
+
+    ap_devs = Container(content=Column(
+        [dd_ap, t2]),
+         width=240,
+         height=150,
+         bgcolor=colors.RED,
+         padding=20)
+
+    ap_output = Container(content=Column(
+        [dd_output, t3]),
+         width=240,
+         height=150,
+         bgcolor=colors.BLUE,
+         padding=20)
+
+    status = Text(" ", size=16)
+    def power_click(e):
+        global all_powr_down, ap_sel, input_sel, ap_internal_sel, \
+            ap_out_sel, output_sel, relay_sel
+
+        # active channels
+        # input_sel
+
+        # print(input_sel, ap_sel, ap_internal_sel, ap_out_sel, output_sel)
+
+        set_spt1(input_sel)
+
+        # set for ap_sel
+        set_spt2(ap_sel)
+        if ap_out_sel > 0:
+            set_spt4(ap_out_sel)
+
+        if ap_internal_sel > 0:
+            set_spt3(ap_internal_sel)
+
+        # set for output_sel
+        set_spt5(output_sel)
+
+        if (Is_output_selected and Is_input_selected and Is_dev_selected):
+            if power_up_button.icon == "pause_circle":
+                power_up_button.icon="play_circle"
+                power_up_button.text="上电"
+                power_up_button.icon_color=colors.GREEN
+                all_powr_down = True
+
+                all_relay_off()
+
+                stop_button.text="已断电"
+                stop_button.style.bgcolor[""]=colors.GREEN
+            else:
+                all_powr_down = False
+                power_up_button.icon="pause_circle"
+                power_up_button.text="断电"
+                power_up_button.icon_color=colors.RED
+
+                stop_button.text="全部断电"
+                stop_button.style.bgcolor[""]=colors.RED
+
+                set_relay(relay_sel)
+
+            status.value = "You chose wisely"
+        else:
+            status.value = "You chose poorly, something is missing"
+
+        page.update()
+
+    def stop_all(e):
+        global all_powr_down
+        if not all_powr_down:
+            all_powr_down = True
+            power_up_button.icon="play_circle"
+            power_up_button.text="上电"
+            power_up_button.icon_color=colors.GREEN
+
+            stop_button.text="已断电"
+            stop_button.style.bgcolor[""]=colors.GREEN
+
+        page.update()
+
+        all_relay_off()
+
+
+    power_up_button = FilledButton(text="上电",
+                                   icon="play_circle",
+                                   icon_color=colors.GREEN,
+                                   on_click=power_click,
+                                   data=False)
+    stop_button = ElevatedButton(
+            "已断电",
+            style=ButtonStyle(shape=CircleBorder(), padding=30,
+                              bgcolor={"":colors.GREEN},
+                              color={"":colors.BLACK}),
+        on_click=stop_all
+        )
+
+    page.add(page_t, header_img, Row([ap_input, ap_devs, ap_output],
+                                     alignment="center"),
+             Row([power_up_button, stop_button], alignment="center"),
+             status
+             )
+
+
+flet.app(port=80, target=main, view=flet.WEB_BROWSER,
+         assets_dir="assets")
+
